d7f0220b7e4416a5424139fda35ae7ef71e41b2f
skorp2910197829@gmail.com <mandarinevgenij29@gmail.com>
2020-03-02 13:52:39 +0200

    change H2

--- spring-binding-data-transaction/src/main/resources/schema.sql
+++ spring-binding-data-transaction/src/main/resources/schema.sql
@@ -1,6 +1,11 @@
-drop database if exists academy;
-create database if not exists academy default character set utf8;
-use academy;
+-- drop database if exists academy;
+-- create database if not exists academy default character set utf8;
+-- use academy;
+create table `groups`(
+                         id int primary key auto_increment,
+                         name varchar(255) not null
+);
+
 create table students (
                           id int primary key auto_increment,
                           first_name varchar(255) not null,
@@ -9,7 +14,3 @@ create table students (
                           `groupId` int,
                           foreign key (`groupId`) references `groups`(id) on delete SET NULL on update cascade
 );
-create table `groups`(
-    id int primary key auto_increment,
-    name varchar(255) not null
-)
